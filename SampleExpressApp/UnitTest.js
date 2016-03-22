94de9869c5effc97afd0b575d6b25545c863bcfc
Jorge <jogonzal@microsoft.com>
2016-03-21 18:44:11 -0700

    Add sample passing UT

---
+++ SampleExpressApp/UnitTest.js
@@ -0,0 +1,17 @@
+﻿var assert = require('assert');
+var Adding = require('./utilities/adding.js');
+
+exports['Test 1'] = function () {
+    assert.ok(true, "This shouldn't fail");
+}
+
+exports['Test 2'] = function () {
+    assert.ok(1 === 1, "This shouldn't fail");
+    assert.ok(false, "This should fail");
+}
+
+exports['Test 3'] = function () {
+    var add = new Adding();
+    var res = add.add(1, 2);
+    assert.ok(res === 3, "1 + 2 = 3");
+}
---
+++ SampleExpressApp/Utilities/Adding.js
@@ -0,0 +1,11 @@
+﻿// Constructor
+function Adding() {
+    // always initialize all instance properties
+    this.author = "Jorge";
+}
+// class methods
+Adding.prototype.add = function (x, y) {
+	return x + y;
+};
+// export the class
+module.exports = Adding;
